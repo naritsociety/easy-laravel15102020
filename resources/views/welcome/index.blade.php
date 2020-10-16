0b692fda3e4efd7bbfc8a551d9e88d18ffd4c058
Narit Charoenval <naritsociety@gmail.com>
2020-10-16 16:42:13 +0700

    Edit indexpage

--- resources/views/welcome/index.blade.php
+++ resources/views/welcome/index.blade.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-
-    </head>
-    <body>
-        <div class="container">
-            <div id="root"></div>
-            <hr />
-            <p>
-                My name is: {{ $name }} today is: {{ $date }}
-            </p>
-        </div>
-        <script src="{{ asset('js/app.js')}}"></script>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <h3>Welcome to Laravel8</h3>
+</div>
+<div id="root"></div>
+@endsection
